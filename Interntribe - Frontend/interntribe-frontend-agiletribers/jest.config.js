7e92508e6c4e2c8abee251c70a1f326aff5d3965
brithiksha nv <136951676+brithikshanv@users.noreply.github.com>
2025-06-03 12:19:38 +0530

    Add files via upload

---
+++ Interntribe - Frontend/interntribe-frontend-agiletribers/jest.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/_*.{js,jsx,ts,tsx}',
+    '!src/**/*.stories.{js,jsx,ts,tsx}',
+    '!src/types/**/*',
+    '!**/node_modules/**',
+    '!**/.next/**',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['json', 'lcov', 'text', 'clover', 'html', 'cobertura'],
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/.next/',
+  ],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['ts-jest', {
+      tsconfig: 'tsconfig.jest.json',
+    }],
+    '^.+\\.css$': '<rootDir>/test/config/cssTransform.js',
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '<rootDir>/test/config/fileTransform.js',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!(react-markdown|remark-gfm|unist|unified|bail|is-plain-obj|trough|vfile|remark|remark-parse|mdast-util|micromark|decode-named-character-reference|character-entities|property-information|space-separated-tokens|comma-separated-tokens|hast-util|html-void-elements|estree-util|devlop)/)',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+  moduleNameMapper: {
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
+}; 
